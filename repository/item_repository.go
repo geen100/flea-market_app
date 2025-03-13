6eceebd48b340c5734f01461cc83694176a3dd7d
geen100 <gen.10.07.yu@icloud.com>
2025-03-13 10:02:18 +0900

    put,delete 実装

--- repository/item_repository.go
+++ repository/item_repository.go
@@ -76,8 +76,17 @@ func (r *ItemRepository) Create(newItem models.Item) (*models.Item, error) {
 }
 
 // Delete implements IItemRepository.
-func (i *ItemRepository) Delete(itemId uint) error {
-	panic("unimplemented")
+func (r *ItemRepository) Delete(itemId uint) error {
+	deleteItem, err := r.FindById(itemId)
+	if err != nil {
+		return err
+	}
+
+	result := r.db.Delete(&deleteItem)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
 
 // FindAll implements IItemRepository.
@@ -94,19 +103,23 @@ func (r *ItemRepository) FindAll() (*[]models.Item, error) {
 // FindById implements IItemRepository.
 func (r *ItemRepository) FindById(itemId uint) (*models.Item, error) {
 	var item models.Item
-	result := r.db.First(&item,itemId)
-	if result.Error != nil{
-		if result.Error.Error() == "record not found"{
+	result := r.db.First(&item, itemId)
+	if result.Error != nil {
+		if result.Error.Error() == "record not found" {
 			return nil, errors.New("item not found")
 		}
-		return nil,result.Error
+		return nil, result.Error
 	}
-	return &item,nil
+	return &item, nil
 }
 
 // Update implements IItemRepository.
-func (i *ItemRepository) Update(updateItem models.Item) (*models.Item, error) {
-	panic("unimplemented")
+func (r *ItemRepository) Update(updateItem models.Item) (*models.Item, error) {
+	result := r.db.Save(&updateItem)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &updateItem, nil
 }
 
 func NewItemRepository(db *gorm.DB) IItemRepository {
